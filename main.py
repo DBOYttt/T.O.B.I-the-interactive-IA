8920f9103b33c8362ea9319e219e319475c5dc1d
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-04-10 10:55:57 +0200

    add first version of tobi
    tobi already has the function of saying hello, I'll ask for your name and compliment it. you can ask him who he is, he will answer that his name is tobi and it's nice to meet you

---
+++ main.py
@@ -0,0 +1,153 @@
+from asyncio import sleep
+from cgitb import text
+from signal import pause
+import time
+import os
+
+print(
+    '''
+██████████████████████████████████████████████████
+█╔╗╔╗╔╦══╦════╦═══╗╪╔╗╪╪╔╦═══╦═══╦════╦═══╦═╗╔═╗╪█
+█║║║║║╠╣╠╣╔╗╔╗║╔═╗║╪║║╪╪║║╔══╣╔═╗║╔╗╔╗║╔══╣║╚╝║║╪█
+█║║║║║║║║╚╝║║╚╣║╪║║╪║║╪╪║║╚══╣╚══╬╝║║╚╣╚══╣╔╗╔╗║╪█
+█║╚╝╚╝║║║╪╪║║╪║╚═╝╠╗║║╔╗║║╔══╩══╗║╪║║╪║╔══╣║║║║║╪█
+█╚╗╔╗╔╬╣╠╗╪║║╪║╔═╗║╚╝║║╚╝║╚══╣╚═╝║╪║║╪║╚══╣║║║║║╪█
+█╪╚╝╚╝╚══╝╪╚╝╪╚╝╪╚╩══╝╚══╩═══╩═══╝╪╚╝╪╚═══╩╝╚╝╚╝╪█
+█╪╪╪╪╪╪╪╪╪╪╪╪╪╔════╦═══╗╔══╗╪╔══╗╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪█
+█╪╪╪╪╪╪╪╪╪╪╪╪╪║╔╗╔╗║╔═╗║║╔╗║╪╚╣╠╝╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪█
+█╪╪╪╪╪╪╪╪╪╪╪╪╪╚╝║║╚╣║╪║║║╚╝╚╗╪║║╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪█
+█╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪║║╪║║╪║║║╔═╗║╪║║╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪█
+█╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪║║╔╣╚═╝╠╣╚═╝╠╦╣╠╗╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪█
+█╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╚╝╚╩═══╩╩═══╩╩══╝╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪█
+█╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪╪█
+██████████████████████████████████████████████████
+'''
+)
+
+time.sleep(2)
+
+def animate_text(text):
+  number_of_characters=1
+  running = True
+  while running == True:
+    print("\n")
+    print(text[0:number_of_characters])
+    number_of_characters += 1
+    if number_of_characters > len(text):
+      number_of_characters = 0
+    time.sleep(0.1) 
+    os.system('clear')
+    if number_of_characters == 16:
+        if running == True:
+           running = False
+        
+animate_text("what's your name?:")
+
+D1 = input("what's your name?:")
+DH = len(D1)
+
+def animate_text(text):
+  number_of_characters=1
+  running = True
+  while running == True:
+    print("\n")
+    print(text[0:number_of_characters])
+    number_of_characters += 1
+    if number_of_characters > len(text):
+      number_of_characters = 0
+    time.sleep(0.1) 
+    os.system('clear')
+    if number_of_characters == DH + 35 :
+        if running == True:
+           running = False
+        
+animate_text(f'{D1} that a nice name, good to see u :)')
+
+print(f'{D1} that a good name, good to see u :)')
+
+D2 = input('Make Question to T.O.B.I:')
+
+if D2.upper().lower() == 'who are u?' : 
+ def animate_text(text):
+   number_of_characters=1
+   running = True
+   while running == True:
+    print("\n")
+    print(text[0:number_of_characters])
+    number_of_characters += 1
+    if number_of_characters > len(text):
+      number_of_characters = 0
+    time.sleep(0.1) 
+    os.system('clear')
+    if number_of_characters == 28 :
+        if running == True:
+           running = False
+ animate_text('im a tobi :) nice to meet u!')
+
+
+if D2.upper().lower() == 'who are you?' : 
+ def animate_text(text):
+   number_of_characters=1
+   running = True
+   while running == True:
+    print("\n")
+    print(text[0:number_of_characters])
+    number_of_characters += 1
+    if number_of_characters > len(text):
+      number_of_characters = 0
+    time.sleep(0.1) 
+    os.system('clear')
+    if number_of_characters == 28 :
+        if running == True:
+           running = False
+ animate_text('im a tobi :) nice to meet u!')
+
+
+if D2.upper().lower() == 'who are you' : 
+ def animate_text(text):
+   number_of_characters=1
+   running = True
+   while running == True:
+    print("\n")
+    print(text[0:number_of_characters])
+    number_of_characters += 1
+    if number_of_characters > len(text):
+      number_of_characters = 0
+    time.sleep(0.1) 
+    os.system('clear')
+    if number_of_characters == 28 :
+        if running == True:
+           running = False
+ animate_text('im a tobi :) nice to meet u!')
+
+
+if D2.upper().lower() == 'who are u' : 
+ def animate_text(text):
+   number_of_characters=1
+   running = True
+   while running == True:
+    print("\n")
+    print(text[0:number_of_characters])
+    number_of_characters += 1
+    if number_of_characters > len(text):
+      number_of_characters = 0
+    time.sleep(0.1) 
+    os.system('clear')
+    if number_of_characters == 28 :
+        if running == True:
+           running = False
+ animate_text('im a tobi :) nice to meet u!')
+
+print('im a tobi :) nice to meet u!')
+
+
+
+
+
+
+
+
+
+
+
+
