81e254afc52570aef374e2db1369d2b73f603d11
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-04-14 10:57:14 +0200

    (fix) in beta and main
    fixed error when u enter something not in dictionary

--- main.py
+++ main.py
@@ -219,8 +219,10 @@ while 1 :
     
   if D2.upper().lower() == 'exit()' :
     break
-  
+ 
   if D2.upper().lower() != x:
+
+   if D2.upper().lower() in z:
     def animate_text(text) : 
       number_of_characters=1
       runing = True
@@ -237,3 +239,21 @@ while 1 :
         if number_of_characters == len(z.get(D2)):
           print(z.get(D2))
     animate_text(z.get(D2))
+  
+  if D2.upper().lower() not in z:
+    def animate_text(text) : 
+      number_of_characters=1
+      runing = True
+      while runing == True:
+        print('\n')
+        print(text[0:number_of_characters])
+        number_of_characters += 1 
+        if number_of_characters > len(text):
+          number_of_characters = 0
+        time.sleep(0.1)
+        os.system('clear')
+        if number_of_characters == len("i can't understund :("): 
+          runing = False 
+        if number_of_characters == len("i can't understund :("):
+          print("i can't understund :(")
+    animate_text("i can't understund :(") 
